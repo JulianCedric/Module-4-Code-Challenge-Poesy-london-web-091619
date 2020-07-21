46b2417a30e1b689cefcc0fb423db9c58fc10e1f
JulianCedric <julian.pormentilla@gmail.com>
2020-07-21 14:54:27 -0400

    Deliverable 1

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+  state = {
+    poems: [],
+    newPoemFormVisible: false
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/poems")
+    .then(resp => resp.json())
+    .then(poems => this.setState({poems}, () => console.log(poems)))
+  }
+
+  handleClick = e => {
+    this.setState({newPoemFormVisible: !this.state.newPoemFormVisible})
+  }
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.handleClick} >Show/hide new poem form</button>
+          {/* {true && <NewPoemForm />} */}
+          {(this.state.newPoemFormVisible ? <NewPoemForm /> : null)}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.poems}/>
       </div>
     );
   }
--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -5,12 +5,14 @@ class PoemsContainer extends React.Component {
   render() {
     return (
       <div className="poems-container">
-        {
-          // render poems here
-        }
+        {this.props.poems.map(poem => <Poem key={poem.id} {...poem}/>)}
       </div>
     );
   }
 }
 
 export default PoemsContainer;
+
+// {...poem} 
+  // same as:
+// id={poem.id} title={poem.title} content={poem.content} author={poem.author}
