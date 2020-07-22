752e5ed8f3937b82642316ff3b5851214cb8e934
JulianCedric <julian.pormentilla@gmail.com>
2020-07-22 01:41:39 -0400

    Some updates / notes -- in progress / continued in practice version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-// 7/21 JP Note: wrt Deliverable 2: In order to toggle the visibility of the NewPOemForm, I needed to add onClick={this.handleClick} to the button element, then define the handleClick function (which updates the newly-created state key that I named, "newPoemFormVisible". 
+// 7/21 JP Note: wrt Deliverable 2: In order to toggle the visibility of the NewPoemForm, I needed to add onClick={this.handleClick} to the button element, then define the handleClick function (which updates the newly-created state key that I named, "newPoemFormVisible". 
+  // also: ternary to toggle show/hide of form
 // 7/21 JP Note: wrt Deliverable 3: In progress - left off at NewPoemForm component.. fetch part.. 
+  // .. continued: To create a poem means to create a new object / hash (of key/value pairs of the values of the input fields), added to the AoH's (all poems data). 
+    // And in order to POST this new poem to both the front-end(virtual to real DOM) AND the back-end (db), we need to: (1) create a function that handles each change (user input) of each input field, and (2) within that function, create another function that handles the new poem OBJECT (which must be defined in App.js and take in the new poem as its argument)  
+
+
+// 7/22 JP Notes: wrt Deliverable 3: breakdown is essentially this: 
+  // state
+  // handleSubmit (referring to the form element's onSubmit={this.handleSubmit})
+  // handleChange (referring to each of the input elements' onChange={this.handleChange})
+// wrtdeliverable 4: update
+  // also: delete button added in practice version 
 
 import React from "react";
 import "./App.css";
@@ -22,13 +33,22 @@ class App extends React.Component {
     this.setState({newPoemFormVisible: !this.state.newPoemFormVisible})
   }
 
+///////////////////////////////////////////////// 
+// we need to pass this function as a prop to the NewPoemForm component:
+
+  handleNewPoem = (newPoem) => {
+    this.setState({poems: [...this.state.poems, newPoem]})
+  }
+
+/////////////////////////////////////////////////
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
           <button onClick={this.handleClick} >Show/hide new poem form</button>
           {/* {true && <NewPoemForm />} */}
-          {(this.state.newPoemFormVisible ? <NewPoemForm /> : null)}
+          {(this.state.newPoemFormVisible ? <NewPoemForm handleNewPoem={this.handleNewPoem} /> : null)}
         </div>
         <PoemsContainer poems={this.state.poems}/>
       </div>
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,44 +1,130 @@
-// 
-
-import React from "react";
+import React from 'react';
 
 class NewPoemForm extends React.Component {
-  state = {
+
+  // this state's keys are e.target.name 
+  // this state's keys' values are e.target.value (USER INPUT per field)
+  state = {  
     id: "",
-    title: "", 
-    content: "",
-    author: ""
+    title: "",
+    author: "",
+    content: ""
   }
 
-  handleNewPoemFormSubmit = e => {
-    e.preventDefault() 
-    fetch("http://localhost:3000/poems", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(
-        {
-          id: this.state.id,
-          title: this.state.title,
-          content: this.state.content,
-          author: this.state.author
-        }
-      )
-    )
-  }}
+///////////////////////////////////////////////// 
+ 
+  handleChange = e => {
+    this.setState({[e.target.namee]: e.target.value})
+  }
 
-  render() {
-    return (
-      <form className="new-poem-form" onSubmit={this.handleNewPoemFormSubmit}>
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
-        <input type="submit" value="Share your masterpiece" />
+///////////////////////////////////////////////// 
+
+  handleSubmit = e => {
+  e.preventDefault()
+  fetch("http://localhost:3000/poems", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(this.state)
+  })
+    .then(resp => resp.json())
+    .then(newPoemObject => this.props.handleNewPoem(newPoemObject), 
+      this.setState({
+        id: "", 
+        title: "", 
+        author: "", 
+        content: ""})
+      )
+  }
+
+///////////////////////////////////////////////// 
+
+  render() { 
+    return (  
+      <form className="new-poem-form" onSubmit={this.handleSubmit}>
+        <input placeholder="id" name="id" value={this.state.id} onChange={this.handleChange} />
+        <input placeholder="Title" name="title" value={this.state.title} onChange={this.handleChange} />
+        <input placeholder="Author" name="author" value={this.state.author} onChange={this.handleChange} />
+        <textarea name="content" placeholder="Write your masterpiece here..." value={this.state.content} onChange={this.handleChange} rows={10} />
+        <button type="submit">Submit Your Poem</button>
       </form>
     );
   }
 }
-
+ 
 export default NewPoemForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 
+
+// import React from "react";
+
+// class NewPoemForm extends React.Component {
+//   state = {
+//     id: "",
+//     title: "", 
+//     content: "",
+//     author: ""
+//   }
+
+//   handleChange = e => {
+//     this.setState({[e.target.name]: e.target.value})
+//   }
+
+//   handleNewPoemFormSubmit = e => {
+//     e.preventDefault() 
+//     fetch("http://localhost:3000/poems", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       },
+//       body: JSON.stringify(
+//         {
+//           id: this.state.id,
+//           title: this.state.title,
+//           content: this.state.content,
+//           author: this.state.author
+//         }
+//       )
+//     }
+//     .then(resp = resp.json())
+//     .then(newPoemObject => this.props.handleNewPoem(newPoemObject),
+//       this.setState({
+//         id: "", 
+//         title: "",
+//         content: "",
+//         author: ""
+//       }))
+//   )
+
+
+//   render(){
+
+//     return (
+//       <form className="new-poem-form" onSubmit={this.handleNewPoemFormSubmit}>
+//         <input name="title" placeholder="Title" value={this.state.title} onChange={this.handleChange} />
+//         <input name="author" placeholder="Author" value={this.state.author} onChange={this.handleChange} />
+//         <textarea name="content" placeholder="Write your masterpiece here..." value={this.state.content} onChange={this.handleChange} rows={10} />
+//         <button type="submit">Submit</button>
+//       </form>
+//     );
+//   };
+// }
+// }
+
+// export default NewPoemForm;
+
